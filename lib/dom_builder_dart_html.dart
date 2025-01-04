d0ed8d7dd40ab005bae15f82b4c3628a5cef4077
gmpassos <gracilianomp@gmail.com>
2025-01-04 20:45:32 -0300

    - Deprecate library `dom_builder_dart_html.dart`.

--- lib/dom_builder_dart_html.dart
+++ lib/dom_builder_dart_html.dart
@@ -1,4 +1,7 @@
-library dom_builder_dart_html;
+/// DOM Builder library, with a `DOMGenerator` over `dart:html` package.
+@Deprecated(
+    "Use `dom_builder_web.dart` (with package `web`). Package `dart:html` is deprecated.")
+library;
 
 export 'dom_builder.dart';
 export 'src/dom_builder_dart_html.dart';
