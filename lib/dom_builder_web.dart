f90c28af454e3878590ed06fe68d1b10771d71c6
gmpassos <gracilianomp@gmail.com>
2025-01-04 20:55:37 -0300

    new library dom_builder_web.dart

---
+++ lib/dom_builder_web.dart
@@ -0,0 +1,6 @@
+/// DOM Builder library, with a `DOMGenerator` over `web` package.
+library;
+
+export 'dom_builder.dart';
+export 'src/dom_builder_web.dart';
+export 'src/dom_builder_generator_web.dart';
